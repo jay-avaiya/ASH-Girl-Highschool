9ad1667832599776639803059bcdbcd813e3fd5e
jay-avaiya <jay@pruthatek.com>
2025-12-07 17:43:19 +0530

    trust body

--- src/pages/Main/TrustbodyPage/TrustbodyPage.jsx
+++ src/pages/Main/TrustbodyPage/TrustbodyPage.jsx
@@ -7,13 +7,13 @@ const TrustbodyPage = () => {
     <div className="flex flex-col items-center gap-36">
       <Banner current={"Trust Body"} />
 
-      <div className="flex flex-col items-center text-center px-14 gap-24">
+      <div className="flex flex-col items-center text-center px-3 md:px-8 lg:px-14 gap-24">
         {/* heading */}
         <div className="flex flex-col items-center space-y-5">
-          <h3 className="text-[34px] font-bold font-montserrat text-[#CC4237]">
+          <h3 className="text-3xl lg:text-[34px] font-bold font-montserrat text-[#CC4237]">
             Managing Committee
           </h3>
-          <p className="text-2xl w-[77%]">
+          <p className="text-[16px] md:text-lg xl:text-2xl lg:w-[77%]">
             At Ash Girls School, our Managing Committee plays a vital role in
             shaping the strategic direction and nurturing the values that define
             our institution. Each member brings a wealth of experience,
@@ -23,7 +23,7 @@ const TrustbodyPage = () => {
         </div>
 
         {/* cards */}
-        <div className="grid grid-cols-4 gap-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-16">
           {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
             <Card key={i} />
           ))}
--- src/pages/Main/TrustbodyPage/_components/Message.jsx
+++ src/pages/Main/TrustbodyPage/_components/Message.jsx
@@ -7,10 +7,10 @@ const Message = () => {
         <img
           src="/quote.png"
           alt="quote"
-          className="w-[60px] h-[60px] 
-          absolute -left-18 -top-7 rotate-2"
+          className="w-[30px] h-[30px] md:w-[45px] md:h-[45px] xl:w-[60px] xl:h-[60px] 
+          absolute -left-12 -top-5 md:-left-14  xl:-left-18 md:-top-7 rotate-2"
         />
-        <h3 className="text-5xl font-montserrat font-extrabold">
+        <h3 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-montserrat font-extrabold">
           Principal Message
         </h3>
       </div>
@@ -18,10 +18,10 @@ const Message = () => {
       <img
         src="/person.png"
         alt="person"
-        className="w-[23%] h-[60vh] rounded-t-full object-cover border-6 border-white"
+        className="w-[70%] md:w-[50%] lg:w-[28%] xl:w-[25%] h-[60vh] rounded-t-full object-cover border-6 border-white"
       />
 
-      <p className="text-2xl w-[45%] text-center">
+      <p className="text-sm md:text-xl lg:text-2xl lg:w-[55%]  xl:w-[45%] text-center">
         Ash Girls School was established with the belief that education is the
         most powerful tool to shape young minds and communities.
       </p>
