096f0a6b9fff83552a25d657410c7ab4ca3c4d0c
jay-avaiya <jay@pruthatek.com>
2025-12-07 15:04:13 +0530

    Gallery Responsive

--- src/pages/Main/Homepage/_components/Gallery.jsx
+++ src/pages/Main/Homepage/_components/Gallery.jsx
@@ -10,13 +10,13 @@ const Gallery = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-12 px-14">
+    <div className="flex flex-col items-center gap-12 px-3 md:px-8 lg:px-14">
       {/* heading */}
       <div className="flex flex-col items-center text-center space-y-3">
-        <h3 className="text-[#CC4237] text-[34px] font-montserrat font-bold">
+        <h3 className="text-[#CC4237] text-3xl lg:text-[34px] font-montserrat font-bold">
           Gallery
         </h3>
-        <p className="text-xl w-[60%]">
+        <p className="text-[16px] md:text-xl lg:text-2xl w-full lg:w-[80%] xl:w-[66%] text-amber-700 md:text-teal-700 lg:text-indigo-600 xl:text-red-700">
           Our school encourages learning through experience. <br /> From
           festivals to competitions, classroom activities to social awareness
           programs — every moment helps students learn, explore, and shine.
